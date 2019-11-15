ac82d1f4f2f05fbd79ebb6d3b8d15798878373af
Max M <maxmcfadden88@gmail.com>
2019-11-15 04:05:03 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,10 +10,11 @@ pp directors_database
 
 def directors_totals(nds)
   result = {}
-  nil
-end
-
-# Find a way to accumulate the :worldwide_grosses and return that Integer
-# using director_data as input
-def gross_for_director(director_data)
+  director_index = 0
+  while director_index < nds.size do
+    director = nds[director_index]
+    result[director[:name]] = gross_for_director(director)
+    director_index += 1
+  end
+  result
 end
